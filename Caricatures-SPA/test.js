40786c23fe71e20c0b511947cb7551c147a29568
PavNedNed <pocomax.studio@gmail.com>
2026-05-06 14:54:33 +0300

    Last upload for the day

---
+++ Caricatures-SPA/test.js
@@ -0,0 +1,18 @@
+let collapseBtn = document.querySelector(".toggle-btn");
+collapseBtn.addEventListener("click", dropTheMenu);
+
+let toggleContainerRef = document.querySelector(".toggle-container");
+let toggleNtnsRef = document.querySelector(".toggle-buttons");
+
+let collapsed = false;
+
+function dropTheMenu(e) {
+    if (!collapsed) {
+        // toggleContainerRef.style.display = "none"
+        toggleContainerRef.classList.remove("show");
+    } else {
+        toggleContainerRef.style.display = "block"
+        toggleContainerRef.classList.add("show");
+    }   
+    collapsed = !collapsed; 
+}
