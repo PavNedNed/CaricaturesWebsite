8aea21cb45a9baeca383897755d64bec96bb0e78
PavNedNed <pocomax.studio@gmail.com>
2026-05-08 15:48:48 +0300

    Modules Upload

---
+++ app.js
@@ -0,0 +1,35 @@
+import page from "./node_modules/page/page.mjs"
+
+import { loadHomeView } from "./src/views/homeView.js"
+import { loadFAQView } from "./src/views/faqView.js";
+import { } from "./src/dropDownMenu.js"
+
+page("/", loadHomeView)
+page("/faq", loadFAQView);
+page();
+
+// // drop-down menu
+// let collapseBtn = document.querySelector(".toggle-btn");
+// collapseBtn.addEventListener("click", dropTheMenu);
+
+// let toggleContainerRef = document.querySelector(".toggle-container");
+// let toggleNtnsRef = document.querySelector(".toggle-buttons");
+
+// let collapsed = true;
+
+// export function setCollapsed(value) {
+//     collapsed = value;
+// }
+
+// function dropTheMenu() {
+//     if (!collapsed) {
+//         toggleContainerRef.classList.remove("show");
+//     } else {
+//         toggleContainerRef.style.display = "block";
+//         toggleContainerRef.classList.add("show");
+//     }
+
+//     collapsed = !collapsed;
+
+//     console.log("mina");
+// }
---
+++ src/views/homeView.js
@@ -0,0 +1,65 @@
+import { html, render } from "../../node_modules/lit-html/lit-html.js"
+import { setCollapsed } from "./../dropDownMenu.js"
+
+let main;
+
+let temp = (dummy) => html `
+    <section class="home-wrapper">
+                <section class="img-container">
+                    <section class="top-text" data-text="РЪЧНО РИСУВАНИ">РЪЧНО РИСУВАНИ</section>
+                    <section class="main-text">КАРИКАТУРИ</section>
+                    <section class="bottom-text" data-text="ПО ПОРЪЧКА">ПО ПОРЪЧКА</section>
+                    <button class="more-btn">НАУЧИ ПОВЕЧЕ</button>
+                </section>
+
+                <hr>
+
+                <section class="top-sections-text-container">
+                    <section class="text-title">
+                        ИЗРАБОТКА НА ДИГИТАЛНИ КАРИКАТУРИ И ПОРТРЕТИ!
+                    </section>
+                    <section class="text-description">
+                        Предлагаме изработката на дигитални карикатури и портрети с помощта на професионални WACOM  графични таблети и специализиран софтуер.
+                    </section>
+                </section>
+
+                <section class="home-sections-wrapper">
+                    <section class="home-section-container">
+                        <section class="home-section-img">
+                            <img src="./images/home/h_1.jpg">
+                        </section>
+                        <section class="home-section-text">
+                            Работим в три фази - скеч, линиране и оцветяване.<br>
+                            Първата фаза започва след като ни споделите Вашата идея и ни изпратите нужните снимки.
+                        </section>
+                        <button class="more-btn">НАШАТА РАБОТА</button>
+                    </section>
+                    <section class="home-section-container">
+                        <section class="home-section-img">
+                            <img src="./images/home/h_2.jpg">
+                        </section>
+                        <section class="home-section-text">
+                            След като сте доволни с крайния резултат, печатаме картините с помощта на принтери с висока резолюция, слагаме ги в рамки и ги изпращаме чрез куриер.
+                        </section>
+                        <button class="more-btn">ПОРТФОЛИО</button>
+                    </section>
+                    <section class="home-section-container">
+                        <section class="home-section-img">
+                            <img src="./images/home/h_4.jpg">
+                        </section>
+                        <section class="home-section-text">
+                            Обикновено използваме <strong><a href="mailto:pocomaxs2@gmail.com" rel="noopener">email кореспонденция</a></strong>, за да общуваме с нашите клиенти, но можете да се свържете с нас и чрез нашата <strong><a href="/contacts">контактна страница</a></strong>.
+                        </section>
+                        <button class="more-btn">КОНТАКТИ</button>
+                    </section>
+                </section>
+            </section>
+`
+
+export function loadHomeView(ctx) {
+    main = document.querySelector("main");
+
+    render(temp("dummy"), main);
+
+    setCollapsed(false);
+}
