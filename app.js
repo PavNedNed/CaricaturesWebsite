75bd4952ab4e87be4f3360203f7c8a6195541612
PavNedNed <pocomax.studio@gmail.com>
2026-05-09 16:58:00 +0300

    New Try

--- app.js
+++ app.js
@@ -4,42 +4,6 @@ import { loadHomeView } from "./src/views/homeView.js"
 import { loadFAQView } from "./src/views/faqView.js";
 import { } from "./src/dropDownMenu.js"
 
-
-// page("#/", loadHomeView)
-// page("#/faq", loadFAQView);
-// page();
-
-window.addEventListener("DOMContentLoaded", () => {
-  page("#/home", loadHomeView);
-  page("#/faq", loadFAQView);
-  page("#/help", loadHelpView);
-  page("#/contacts", loadContactsView);
-
-  page();
-});
-
-// // drop-down menu
-// let collapseBtn = document.querySelector(".toggle-btn");
-// collapseBtn.addEventListener("click", dropTheMenu);
-
-// let toggleContainerRef = document.querySelector(".toggle-container");
-// let toggleNtnsRef = document.querySelector(".toggle-buttons");
-
-// let collapsed = true;
-
-// export function setCollapsed(value) {
-//     collapsed = value;
-// }
-
-// function dropTheMenu() {
-//     if (!collapsed) {
-//         toggleContainerRef.classList.remove("show");
-//     } else {
-//         toggleContainerRef.style.display = "block";
-//         toggleContainerRef.classList.add("show");
-//     }
-
-//     collapsed = !collapsed;
-
-//     console.log("mina");
-// }
+page("#/home", loadHomeView);
+page("/faq", loadFAQView);
+page();
