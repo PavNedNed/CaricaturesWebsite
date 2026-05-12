5aca6ea38c799210d84042d12e6ae0ae7b6d4970
PavNedNed <pocomax.studio@gmail.com>
2026-05-12 20:31:59 +0300

    slash

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { loadHomeView } from "./src/views/homeView.js"
 import { loadFAQView } from "./src/views/faqView.js";
 import { } from "./src/dropDownMenu.js"
 
-page.base('/CaricaturesWebsite');
+page.base('/CaricaturesWebsite/');
 
 // This handles the "empty" path after CaricaturesWebsite/
 page('/', () => page.redirect('/home')); 
